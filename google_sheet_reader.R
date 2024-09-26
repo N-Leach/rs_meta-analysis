ecbea9d7482b35fd4fad15d958b079c4d2cec65e
N-Leach <nina.leach@icloud.com>
2024-09-26 20:22:42 +0200

    wd problem hopefully fixed

---
+++ google_sheet_reader.R
@@ -0,0 +1,12 @@
+# Load used packages
+library(googlesheets4)
+library(gargle)
+
+# Get Google Drive authorization token from the GitHub secrets vault
+gs4_auth(path = Sys.getenv("GOOGLE_SERVICE_ACCOUNT_KEY"))
+
+
+# Read data from Google Sheet
+sheet_url <- "https://docs.google.com/spreadsheets/d/1weav3qdydHXLBYVfejVZH2wwSYKBZP9Xjb2y0Iw_q5Q/edit?gid=1068904194#gid=1068904194"
+data <- read_sheet(sheet_url)
+
