63671aee1b69463f1caee7a7ee41e9ea76182b16
N-Leach <nina.leach@icloud.com>
2024-09-26 23:20:28 +0200

    test2

--- google_sheet_reader.R
+++ google_sheet_reader.R
@@ -4,7 +4,7 @@ library(gargle)
 
 # Get Google Drive authorization token from the GitHub secrets vault
 gs4_auth(path = Sys.getenv("GOOGLE_SERVICE_ACCOUNT_KEY"))
-
+#gs4_auth(path = '_google_cred/re-meta-analysis-6db69afa8c71.json')
 
 # Read data from Google Sheet
 sheet_url <- "https://docs.google.com/spreadsheets/d/1weav3qdydHXLBYVfejVZH2wwSYKBZP9Xjb2y0Iw_q5Q/edit?gid=1068904194#gid=1068904194"
